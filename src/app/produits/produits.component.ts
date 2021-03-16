3f68b5ca3e36f360e05657e3b7a8a566908483d2
Donovan <donogf@hotmail.fr>
2021-03-17 00:04:52 +0100

    Observable produit

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of, from} from 'rxjs';
 
 @Component({
   selector: 'app-produits',
@@ -9,11 +9,36 @@ import {Observable} from 'rxjs';
 export class ProduitsComponent implements OnInit {
 
   observable$ : Observable<string>;
+  tabData : Array<String> =[];
+  subscribe : any;
 
+  observable2$ : Observable<Array<string>> = of (["Eau","Pain","Steak","Frites","Yaourt"])
 
   constructor() { }
 
   ngOnInit(): void {
+    this.observable$ = new Observable(
+      observer => {
+        observer.next ("Data 1");
+        observer.next ("Data 2");
+        observer.next ("Data 3");
+        observer.complete();
+      }
+    )
   }
 
+  onClick(){
+
+    if (this.subscribe){
+      this.subscribe.unsubscribe();
+    }
+
+    this.subscribe = this.observable2$.subscribe (
+      {
+      next : value => {this.tabData.push(value)},
+      complete : () => {console.log ("complete")},
+      error : err => {console.log(err)}
+      }
+    )
+  }
 }
